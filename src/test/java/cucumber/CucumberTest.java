3320ce871a787381e5ed04ae0c9ecd9ba7619f8e
arturganeev <arturganeev@list.ru>
2020-01-09 16:30:59 +0300

    Refactor feature files.

---
+++ src/test/java/cucumber/CucumberTest.java
@@ -0,0 +1,14 @@
+package cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        monochrome = true,
+        features = {"src/test/resources/"}
+)
+public class CucumberTest {
+
+}
